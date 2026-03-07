2257eef00522e0da098290a9e13b78f6e24cf8bb
Halan <halanbm98@gmail.com>
2026-03-07 13:05:32 -0800

    Age Mid + Sex based differences graphics done, only region Differences left to proceed with frontend for Portafolio

--- survival_cancer/scraping.py
+++ survival_cancer/scraping.py
@@ -41,19 +41,11 @@ pd.set_option("display.max_colwidth", None)
 #print(df.tail(2)) #Last x values
 
 #**************INSIGHTS****************#
-
 #Insights to dataframe to later create graphics
-
-
 #Filter and leave only where Charasteristics = 5 year net survival and also only keep UOM = percenteg
-
 filtered_df= final_df[(final_df['Characteristics']== '5-year net survival') & (final_df['UOM']== 'Percentage')].copy()
-
 filtered_df['cancer_type'] = filtered_df['cancer_type'].str.replace(r'\[.*\]','',regex=True)
-#print(filtered_df.head(5))
-
 #Cancer-Type Survival in 5 year net survival
-
 cancer_survival_df = filtered_df.groupby(['cancer_type'])['survival_rate'].mean().reset_index()
 cancer_survival_df = cancer_survival_df.sort_values('survival_rate',ascending=False) #Descending order
 cancer_survival_df['rank'] = cancer_survival_df['survival_rate'].rank(ascending=False,method='dense') #create rank column
@@ -66,37 +58,83 @@ graph_can_surv = px.bar(
     orientation = 'h',
     color = 'rank',
     text = 'survival_rate', #add label on top of the bars
-    title = 'Average Survival rate by Cancer Type' #head of the graphic
+    title = 'Average Survival rate by Cancer Type', #head of the graphic
+    labels={
+        'survival_rate':'Survival Rate (%)',
+        'cancer_type':'Cancer Type',
+        'rank':'Ranking'
+    }
 )
-
 #Showing yaxe ascending
-#graph_can_surv.update_yaxes(categoryorder='total ascending')
-
 graph_can_surv.show()
-
-#print('Cancer Type Survival rate mean \n',cancer_survival_df.head(20))
-
-#Sex based Differences
-
+#Sex based Differences per Type of cancer
+filtered_df = filtered_df[filtered_df['Sex'] != 'Both sexes'] #Delete Sex=both sexes
 sex_differences_df = filtered_df.groupby(['cancer_type','Sex'])['survival_rate'].mean().reset_index()
-sex_differences_df.sort_values(by='survival_rate', ascending=False)
-#print('Sex based differences survival rate mean \n',sex_differences_df.head(5))
+#reshape column sex into  female and male colums using pivot() -> turn rows into columns
+sex_differences_df = sex_differences_df.pivot(
+    index='cancer_type',
+    columns='Sex',
+    values='survival_rate'
+)
+sex_differences_df['differences'] = sex_differences_df['Females'] - sex_differences_df['Males']  
+sex_differences_df = sex_differences_df.reset_index()#change it back to number indexes insteand of cancer type
+sex_differences_df = sex_differences_df.head(10)
+sex_differences_df = sex_differences_df.sort_values(by='differences',ascending=False)
+#Graphic females vs males by cancer type 
+graph_sex_surv_a = px.bar(
+    data_frame = sex_differences_df,
+    x = ['Females','Males'],
+    y = 'cancer_type',
+    barmode='group',
+    orientation = 'h',
+    title = 'Sex  Survival rate Difference by Cancer Type', #head of the graphic
+)
+graph_sex_surv_a.update_layout(#Change name of axis title
+    xaxis_title = 'Survival Rate (%)',
+    yaxis_title = 'Cancer Type'
+)
+#graph_sex_surv_a.show()
+#Graphic difference survival rate female vs male
+graph_sex_surv_b = px.bar(
+    data_frame = sex_differences_df,
+    x = 'differences',
+    y = 'cancer_type',
+    orientation = 'h',
+    title = 'Difference Survival rate Female vs Male by Cancer Type', #head of the graphic
+    labels = {
+        'differences':'Survival Rate Difference (%)',
+        'cancer_type':'Cancer Type'
+    }
+)
+#graph_sex_surv_b.show()
 
 #Age Group
 #Clean Age Group data with regular expresions -> (\d+)\s+to\s+(\d+)
-
-df_age = filtered_df[filtered_df['age_group'].str.contains('Total')==False].copy()
-
-df_age[['age_min','age_max']] = df_age['age_group'].str.extract(r'(\d+)\s+to\s+(\d+)') #r means raw strign 
-
-df_age['age_min'] = df_age['age_min'].astype(int)
-df_age['age_max'] = df_age['age_max'].astype(int)
+df_age = filtered_df[filtered_df['age_group'].str.contains('Total')==False].copy()#deleting Total from the string
+df_age[['age_min','age_max']] = df_age['age_group'].str.extract(r'(\d+)\s+to\s+(\d+)') #r means raw strign - extracted the numbers of the string
+df_age['age_min'] = df_age['age_min'].astype(int) #values to integer
+df_age['age_max'] = df_age['age_max'].astype(int)#values to integer
 df_age['age_mid'] = (df_age['age_min'] + df_age['age_max'])/2 #mid point of ages
-
 age_group_df = df_age.groupby(['cancer_type','age_mid'])['survival_rate'].mean().reset_index()
-age_group_df.sort_values(by='survival_rate',ascending=False)
+age_group_df = age_group_df.sort_values(by='age_mid',ascending=True)
+age_group_df = age_group_df.head(40)#work with only 40 examples
 
-#print("Age Group Survival Rate mean \n", age_group_df.head(5))
+graph_age_surv = px.line(
+    data_frame=age_group_df,
+    x= 'age_mid',
+    y='survival_rate',
+    orientation = 'v',
+    color='cancer_type',
+    markers=True,
+    title = 'Age Mid Point Survival Rate by Cancer Type',
+    labels={
+        'age_mid' : 'Age Mid Point',
+        'survival_rate' : 'Survival Rate (%)',
+        'cancer_type' : 'Cancer Type'
+    }
+)
+#graph_age_surv.show()
+
 #Regional Diferences
 region_dif_df = filtered_df.groupby(['Region','cancer_type'])['survival_rate'].mean().reset_index()
 region_dif_df.sort_values(by='survival_rate',ascending=False)
